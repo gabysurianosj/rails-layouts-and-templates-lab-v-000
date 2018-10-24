a779774b2994b2bfc0588dbfbb502aed846478b5
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-24 22:41:15 +0000

    automatically backed up by learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,11 +1,14 @@
 class StoreAdminController < ApplicationController
   layout "admin"
-   def home
+
+  def home
   end
-   def orders
+
+  def orders
     render :layout => "order_administration"
   end
-   def invoice
+
+  def invoice
     render :layout => false
   end
-end 
+end
